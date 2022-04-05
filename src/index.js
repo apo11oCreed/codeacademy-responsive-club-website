a9272f67dfd04cb3fbbe3c79848631b4ad134a38
nc <nelson.ncdesigns@gmail.com>
2022-04-05 13:00:20 -0400

    Club landing page complete with responsiveness and SVG filters

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 //console.log("Hello World!");
-import('./css/styles.styl');
-import logo from './img/apple-touch-icon.png';
-import $ from "jquery";
+import './css/styles.styl'
+import logo from './img/apple-touch-icon.png'
+import imgTheraputic from './img/rifqi-ali-ridho-VlXlW77swwY-unsplash.jpg'
+import imgCraftsmanship from './img/roman-kraft-ZdOEgR6pn-A-unsplash.jpg'
+import imgIntroduction from './img/alice-dietrich-FwF_fKj5tBo-unsplash.jpg'
+import $ from "jquery"
 
 var jq = $.noConflict();
 (function($){
+
     $('.logo > a > img').attr('src',logo);
+    $('.painting > svg > image').attr('xlink:href', imgTheraputic);
+    $('#craftsmanship img').attr('src', imgCraftsmanship);
+    $('#introduction img').attr('src', imgIntroduction);
+    
 })(jq);
+
